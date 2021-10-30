de58be9649ab0fd2b9920de7bb4bef2557428449
Bohdan881 <6228238-Bohdan881@users.noreply.gitlab.com>
2021-10-30 13:59:29 -0400

    feat: setup table

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+/* Constants that are shared across the app */
+
+// API
+
+export const MOVIE_LINK_API =
+  "https://skyit-coding-challenge.herokuapp.com/movies";
+
+export const API_STATUS = {
+  IDLE: "idle",
+  SUCCESS: "success",
+  LOADING: "loading",
+  ERROR: "error",
+};
+
+export const RATE_MAX = 5;
